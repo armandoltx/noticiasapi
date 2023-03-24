10d1b889858d2de0ea4336e28d6524e7186af130
Armando Ruiz <armando.ruiz@industrie.co>
2023-03-24 14:51:05 +1100

    340. Consultando las noticias en la api con axios segun la categoria seleccionada

--- src/context/NoticiasProvider.jsx
+++ src/context/NoticiasProvider.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import axios from 'axios'
 import { useState, useEffect, createContext } from 'react'
 
 const NoticiasContext = createContext()
 
 const NoticiasProvider = ({children}) => {
    const [categoria, setCategoria] = useState('general')
+   const [noticias, setNoticias] = useState('')
+
+   useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=mx&category=${categoria}&apiKey=${import.meta.env.VITE_API_KEY}`
+
+      const { data } = await axios(url)
+      // console.log(data)
+      setNoticias(data.articles)
+    }
+
+    consultarAPI()
+   },[categoria]) // Cada vez q cambie q haga una consulta
 
    const handleChangeCategoria = e => {
     setCategoria(e.target.value)
